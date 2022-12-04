df7832966e64bb28e87cb0e387d411f39a8255a2
Felix <felixokorie01@gmail.com>
2022-12-04 13:23:07 -0800

    update 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -12,7 +12,7 @@ def new_in_list(my_list, idx, element):
         The return value. list
     """
     if idx < 0 or idx >= len(my_list):
-        return my_list
+        return my_list.copy()
     new_list = my_list.copy()
     new_list[idx] = element
     return new_list
