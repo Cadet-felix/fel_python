64def811bd152af7831220fb344df747e857b31b
Felix <felixokorie01@gmail.com>
2022-12-07 08:35:58 -0800

    update tast 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     result = 0
     new_list = set(my_list)
 
-    for elem in range(len(new_list)):
-        result += new_list[elem]
+    for elem in new_list:
+        result += elem
 
     return result
